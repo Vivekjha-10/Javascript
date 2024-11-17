f9b363ac323b06f9e533a5b89240f753ebfe8285
Vivek Jha <vivekjha.dg@outlook.com>
2024-11-17 18:34:57 +0530

    variable

---
+++ 01Basics/01_Variable.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "Vivek@google.com"
+var accountPassword = "12345"
+accountCity = "Nashik"
+
+// accountId =2 // not allowed  do not change value  
+
+accountEmail = "viv@.com"
+accountPassword = "778899"
+accountCity = "Pune"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
