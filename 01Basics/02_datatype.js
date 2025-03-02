52fe7708611ef536b4ff34220f986516e4db953d
Vivekjha-10 <jhavivekkumar81@gmail.com>
2025-03-02 19:46:19 +0530

    add datatype

---
+++ 01Basics/02_datatype.js
@@ -0,0 +1,22 @@
+let name = "vivek"
+let age = 22
+let isLoggedIn = false
+
+
+// number = 2 to power 53
+// bigint
+// string => ""
+// bollean => true/false
+// null => standalone value
+// undefined =>
+// symbol
+
+
+
+// object
+
+console.log(typeof undefined);  //undefine
+console.log(typeof null); // object
+
+
+
